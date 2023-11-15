5b22ab4c9a0ccf36ae77ca2bc813e152a5c2ad55
Kevin Becker <becker8@hm.edu>
2023-11-15 11:44:59 +0100

    minMaxScalerNode

--- src/nodes/minMaxScaler.rs
+++ src/nodes/minMaxScaler.rs
@@ -3,13 +3,13 @@ use flowrs::RuntimeConnectable;
 
 use ndarray::{Array2, array};
 use linfa::dataset::DatasetBase;
-use linfa_reduction::Pca;
-use linfa::traits::{Fit, Predict};
+use linfa::traits::{Fit, Transformer};
+use linfa_preprocessing::linear_scaling::LinearScaler;
 use serde::{Deserialize, Serialize};
 
 
 #[derive(RuntimeConnectable, Deserialize, Serialize)]
-pub struct PCANode {
+pub struct MinMaxScalerNode {
     #[output]
     pub output: Output<Array2<f64>>,
 
@@ -17,7 +17,7 @@ pub struct PCANode {
     pub input: Input<Array2<f64>>,
 }
 
-impl PCANode {
+impl MinMaxScalerNode {
     pub fn new(change_observer: Option<&ChangeObserver>) -> Self {
         Self {
             output: Output::new(change_observer),
@@ -26,27 +26,23 @@ impl PCANode {
     }
 }
 
-impl Node for PCANode {
+impl Node for MinMaxScalerNode {
     fn on_update(&mut self) -> Result<(), UpdateError> {
 
         if let Ok(data) = self.input.next() {
-            println!("JW-Debug PCANode has received: {}.", data);
+            println!("JW-Debug MaxAbsScalerNode has received: {}.", data);
 
             // transform to DatasetBase
             let dataset = DatasetBase::from(data);
 
-            // parameter
-            let embedding_size = 2;
-            // pca
-            let embedding = Pca::params(embedding_size)
-                .fit(&dataset)
-                .unwrap();
-            let reduced_dataset = embedding.predict(dataset);
-            
-            // debug
-            println!("Pca: {}", reduced_dataset.targets);
+            // max abs scaling
+            let scaler = LinearScaler::min_max().fit(&dataset).unwrap();
+            let dataset = scaler.transform(dataset);
 
-            self.output.send(reduced_dataset.targets).map_err(|e| UpdateError::Other(e.into()))?;
+            // debug
+            println!("Scaled data: {}", dataset.records);
+
+            self.output.send(dataset.records).map_err(|e| UpdateError::Other(e.into()))?;
         }
         Ok(())
     }
@@ -66,22 +62,22 @@ fn input_output_test() -> Result<(), UpdateError> {
                                          [4.0, 5.0, 6.0, 7.0, 8.0, 9.0], 
                                          [10.0, 11.0, 12.0, 13.0, 14.0, 15.0]];
 
-    let mut and: PCANode<> = PCANode::new(Some(&change_observer));
+    let mut and: MinMaxScalerNode<> = MinMaxScalerNode::new(Some(&change_observer));
     let mock_output = flowrs::connection::Edge::new();
     flowrs::connection::connect(and.output.clone(), mock_output.clone());
     and.input.send(test_input)?;
     and.on_update()?;
 
-    let expected: Array2<f64> = array![[-3.076047733203457, -10.562293260063301],
-                                       [-3.561730416569943, 3.951032231750752],
-                                       [14.63575200500477, 1.1072539713398344], 
-                                       [-3.347031741680441, -4.147375003300382],
-                                       [-4.622799446757189, 10.4931265494172],
-                                       [-2.709147889142067, -11.467625779659173],
-                                       [-3.984915594218815, 3.1728757730584096],
-                                       [14.63575200500477, 1.1072539713398344],
-                                       [-3.347031741680441, -4.147375003300382],
-                                       [-4.622799446757189, 10.4931265494172]];
+    let expected: Array2<f64> = array![[0.00833333333333334, 0.041666666666666664, 0.01666666666666668, 0.29999999999999993, 0.26666666666666666, 0.30833333333333335],
+                                       [0.5666666666666667, 0.5166666666666666, 0.5416666666666666, 0.775, 0.75, 0.75],
+                                       [1., 1., 1., 0., 0., 0.],
+                                       [0.25, 0.25, 0.25, 0.5, 0.5, 0.5],
+                                       [0.75, 0.75, 0.75, 1., 1., 1.],
+                                       [0., 0., 0., 0.25, 0.25, 0.25],
+                                       [0.5, 0.5, 0.5, 0.75, 0.75, 0.75],
+                                       [1., 1., 1., 0., 0., 0.],
+                                       [0.25, 0.25, 0.25, 0.5, 0.5, 0.5],
+                                       [0.75, 0.75, 0.75, 1., 1., 1.]];
     let actual: Array2<f64> = mock_output.next()?;
 
     Ok(assert!(expected == actual))
