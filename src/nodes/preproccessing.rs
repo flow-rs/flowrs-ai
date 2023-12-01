2b88d0edfd3d866fd3921fb5eededbb85a7097a6
MarHof78 <mhofbaue@hm.edu>
2023-12-01 15:43:42 +0100

    checking and changing data types so it is compatible with output of the camera and input of the model node. Should be functional in the flow

--- src/nodes/preproccessing.rs
+++ src/nodes/preproccessing.rs
@@ -14,20 +14,16 @@ use ndarray::{
     Array
 };
 
-use image::{GenericImageView, imageops::FilterType};
+use image::{GenericImageView, imageops::FilterType, DynamicImage};
 
-#[derive(Clone, Deserialize, Serialize, Debug)]
-pub struct ModelConfig {
-   pub model_path: String
-}
 
 #[derive(RuntimeConnectable, Deserialize, Serialize)]
 pub struct PreProccessingNode
 {
     #[input]
-    pub input: Input<i32>,
+    pub input: Input<DynamicImage>,
     #[output]
-    pub output: Output<i32>,
+    pub output: Output<ArrayD<f32>>,
 }
 
 impl PreProccessingNode
@@ -51,21 +47,24 @@ impl Node for PreProccessingNode
     }
 }
 
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
+
 fn preproccessing_input() -> Result<ArrayBase<OwnedRepr<f32>, Dim<[usize; 4]>>, UpdateError>{
     let input_path = "C:/Users/Marcel/LRZ Sync+Share/Master/3_Semester/Hauptseminar_2/flow-rs/flowrs-ai/src/example_pic/crosswalk.jpg";
     let img = image::open(input_path).expect("Failed to open image"); // img = input
+    //let img = self.input;
 
-    let constant: i32 = 1;
-    let rgb: i32 = 3;
-    let squeezenet_width: u32 = 224;
-    let squeezenet_height: u32 = 224;
+    let squeezenet_width: usize = 224;
+    let squeezenet_height: usize = 224;
 
-    let resized_image = img.resize_exact(squeezenet_width, squeezenet_height, FilterType::CatmullRom);
+    let resized_image = img.resize_exact(squeezenet_width as u32, squeezenet_height as u32, FilterType::CatmullRom);
 
-    let dim = Dim((1,3,224, 224));
-
+    let dim = Dim((1,3,squeezenet_width, squeezenet_height));
+    print_type_of(&squeezenet_height);
     let mut input_tensor: Array4<f32> = Array::zeros(dim);
-
+    print_type_of(&input_tensor);
      
     for pixel in resized_image.pixels() {
         let x = pixel.0 as usize;
@@ -75,7 +74,7 @@ fn preproccessing_input() -> Result<ArrayBase<OwnedRepr<f32>, Dim<[usize; 4]>>, 
         input_tensor[[0, 1, y, x]] = (g as f32) / 255.0;
         input_tensor[[0, 2, y, x]] = (b as f32) / 255.0;
     };
-    println!("{:?}", input_tensor);
+    print_type_of(&input_tensor);
     
     Ok(input_tensor)
 }
