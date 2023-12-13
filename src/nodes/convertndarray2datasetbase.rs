12d27a1d3ab4cabf2fffedfcce74472b53cd1471
Johannes Walter <jwalter@hm.edu>
2023-12-13 13:09:21 +0100

    Fixed unit test assert

--- src/nodes/convertndarray2datasetbase.rs
+++ src/nodes/convertndarray2datasetbase.rs
@@ -6,21 +6,16 @@ use ndarray::prelude::*;
 use serde::{Deserialize, Serialize};
 use linfa::prelude::*;
 
-
-// Definition eines Structs
 #[derive(RuntimeConnectable, Deserialize, Serialize)]
-pub struct ConvertNdarray2DatasetBase { // <--- Wenn man eine neue Node anlegt, einfach alles kopieren und hier den Namen ändern
+pub struct ConvertNdarray2DatasetBase { 
     #[input]
-    pub input: Input<Array2<f64>>, // <--- Wir haben in diesem Fall eine Output-Variable vom Typ Array2<u8>
+    pub input: Input<Array2<f64>>,
 
     #[output]
-    //pub output: Output<DatasetBase<(), ndarray::ArrayBase<ndarray::OwnedRepr<f64>, ndarray::Dim<[usize; 2]>>>>, // <--- Wir haben in diesem Fall eine Output-Variable vom Typ Array2<u8>
     pub output: Output<DatasetBase<Array2<f64>, Array1<()>>>
 }
 
-// Das ist einfach der Konstruktur
 impl ConvertNdarray2DatasetBase {
-    // Hier will der Konstruktur als einzigen Parameter einen ChangeObserver
     pub fn new(change_observer: Option<&ChangeObserver>) -> Self {
         Self {
             output: Output::new(change_observer),
@@ -29,37 +24,20 @@ impl ConvertNdarray2DatasetBase {
     }
 }
 
-// Hier befinden sich die Methoden von unserer Node. Wir verwenden erstmal nur die Methoden, welche wir implementieren müssen, da diese von "Node" vorgegeben werden.
 impl Node for ConvertNdarray2DatasetBase {
-    // on_update wird von der Pipeline automatisch getriggert, wenn diese Node einen Input bekommt.
     fn on_update(&mut self) -> Result<(), UpdateError> {
 
-        // Hier überprüfen wir nur, ob ein input da ist und der passt
         if let Ok(node_data) = self.input.next() {
-            //println!("JW-Debug: ConvertNdarray2DatasetBase has received: {}.", node_data);
             println!("JW-Debug: ConvertNdarray2DatasetBase has received an update!");
 
             let dataset = Dataset::from(node_data.clone());
 
-            /*println!("DatasetBase\n");
-            println!("Records:\n {}\n", dataset.records);
-            println!("Targets:\n {:?}\n", dataset.targets);
-            println!("Feature names:\n {:?}\n", dataset.feature_names());*/
-
-            // Hier schicken wir node_data als output an die nächste node bzw. den output
             self.output.send(dataset).map_err(|e| UpdateError::Other(e.into()))?;
         }
         Ok(())
     }
 }
 
-
-// #############################################################################
-// #############################################################################
-// Test, um die Node zu testen
-// Hier auf "|> Run Test" drücken, was unter "#[test" angezeigt wird
-// #############################################################################
-// #############################################################################
 #[test]
 fn input_output_test() -> Result<(), UpdateError> {
     let change_observer = ChangeObserver::new();
@@ -72,8 +50,7 @@ fn input_output_test() -> Result<(), UpdateError> {
     test_node.on_update()?;
 
     let actual: DatasetBase<ArrayBase<OwnedRepr<f64>, Dim<[usize; 2]>>, ArrayBase<OwnedRepr<()>, Dim<[usize; 1]>>> = mock_output.next()?;
+    let expected: Array2<f64> = array![[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]];
 
-
-
-    Ok(assert!(true))
+    Ok(assert!(expected == actual.records))
 }
