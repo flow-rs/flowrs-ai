ed60a792b2b51a59a11fa9f28f1a465a3b96553f
Bellhopx <temp_bellhop@outlook.com>
2023-12-13 13:29:47 +0100

    add label encoder -> functions now

--- src/nodes/csvToDsBWithStringEncoder.rs
+++ src/nodes/csvToDsBWithStringEncoder.rs
@@ -126,34 +126,61 @@ where
                     }
                 }
 
+                let mut label_encoded_ordinals: Vec<Vec<f64>> = Vec::new();
+                for ordinal in ordinal_data.iter() {
+                    label_encoded_ordinals.push(label_encode(&ordinal));
+                }
+
                 // Converting Data to ndarrays:
                 let nominal_records: Array2<String> = Array2::from_shape_vec((nominal_data.len(), headers.len()), nominal_data.into_iter().flatten().collect()).unwrap();
-                let ordinal_records: Array2<String> = Array2::from_shape_vec((ordinal_data.len(), headers.len()), ordinal_data.into_iter().flatten().collect()).unwrap();
+                let ordinal_records: Array2<f64> = Array2::from_shape_vec((label_encoded_ordinals.len(), headers.len()), label_encoded_ordinals.into_iter().flatten().collect()).unwrap();
                 let other_records: Array2<f64> = Array2::from_shape_vec((other_data.len(), headers.len()), other_data.into_iter().flatten().collect()).unwrap();
                                 
+
+
                 // Label Encoding ordinal records
                 //let label_encoded_ordinals = label_encode_ordinal(&ordinal_records);
 
                 // One hot encoding nominal_records
-                let (one_hot_encoded_nominals, nominal_feature_names) = one_hot_encode(&nominal_records);
-                println!("nominal features: {:?}", nominal_feature_names);
-                println!("One hot encoding: {:?}", one_hot_encoded_nominals);
+                //let (one_hot_encoded_nominals, nominal_feature_names) = one_hot_encode(&nominal_records);
+                //println!("nominal features: {:?}", nominal_feature_names);
+                //println!("One hot encoding: {:?}", one_hot_encoded_nominals);
 
                 // Converting ndarrays to DatasetBase
-                let nominals_dbs:DatasetBase<ArrayBase<OwnedRepr<f64>, Dim<[usize; 2]>>, ArrayBase<OwnedRepr<()>, Dim<[usize; 1]>>> = DatasetBase::from(one_hot_encoded_nominals.clone());
-                let nominals:DatasetBase<ArrayBase<OwnedRepr<f64>, Dim<[usize; 2]>>, ArrayBase<OwnedRepr<()>, Dim<[usize; 1]>>> = nominals_dbs.with_feature_names(nominal_feature_names);
+                //let nominals_dbs:DatasetBase<ArrayBase<OwnedRepr<f64>, Dim<[usize; 2]>>, ArrayBase<OwnedRepr<()>, Dim<[usize; 1]>>> = DatasetBase::from(one_hot_encoded_nominals.clone());
+                //let nominals:DatasetBase<ArrayBase<OwnedRepr<f64>, Dim<[usize; 2]>>, ArrayBase<OwnedRepr<()>, Dim<[usize; 1]>>> = nominals_dbs.with_feature_names(nominal_feature_names);
                 //let ordinals_dbs:DatasetBase<ArrayBase<OwnedRepr<f64>, Dim<[usize; 2]>>, ArrayBase<OwnedRepr<()>, Dim<[usize; 1]>>> = DatasetBase::from(label_encoded_ordinals.clone());
                 //let ordinals:DatasetBase<ArrayBase<OwnedRepr<f64>, Dim<[usize; 2]>>, ArrayBase<OwnedRepr<()>, Dim<[usize; 1]>>> = ordinals_dbs.with_feature_names(config.ordinals);
-                let others_dbs:DatasetBase<ArrayBase<OwnedRepr<f64>, Dim<[usize; 2]>>, ArrayBase<OwnedRepr<()>, Dim<[usize; 1]>>> = DatasetBase::from(other_records.clone());
-                let others = others_dbs.with_feature_names(config.others);
+                //let others_dbs:DatasetBase<ArrayBase<OwnedRepr<f64>, Dim<[usize; 2]>>, ArrayBase<OwnedRepr<()>, Dim<[usize; 1]>>> = DatasetBase::from(other_records.clone());
+                //let others = others_dbs.with_feature_names(config.others);
 
-                println!("Nominals: {:?}", nominals);
+                println!("Nominals: {:?}", nominal_records);
                 println!("Ordinal records: {:?}", ordinal_records);
                 //println!("Ordinals: {:?}", ordinals);
-                println!("Others: {:?}", others);
+                println!("Others: {:?}", other_records);
 
                 //let nominal_dataset:DatasetBase<_, _> = DatasetBase::from(nominal_records);
                 //let other_dataset: DatasetBase<f64, f64> = DatasetBase::from(Records::new(other_records, None));
+
+
+
+                // load data in a vector
+                let data: Vec<String> = vec!["hello".to_string(),
+                "world".to_string(),
+                "world".to_string(),
+                "world".to_string(),
+                "world".to_string(),
+                "again".to_string(),
+                "hello".to_string(),
+                "again".to_string(),
+                "goodbye".to_string()];
+
+                // Apply label encoding
+                let encoded_labels = label_encode(&data);
+
+                // Display the result
+                println!("Encoded Labels: {:?}", encoded_labels);
+                
 
 
 
@@ -176,7 +203,7 @@ where
 #[test]
 fn input_output_test() -> Result<(), UpdateError> {
     let change_observer = ChangeObserver::new();
-    let test_data_input = String::from("Feate1,Feature2,Feature3,F4,F5,F6\n1,2,3,1,2,3\n4,5,6,1,2,3\n7,8,9,1,2,3\n10,8,9,1,2,3\n12,8,9,1,2,3\n7,8,9,1,2,3");
+    let test_data_input = String::from("Feate1,Feature2,Feature3,F4,F5,F6\n1,2,3,1,2,11\n4,5,6,1,5,3\n7,8,9,1,2,3\n10,8,9,1,6,4\n12,8,9,1,2,3\n7,8,9,1,2,3");
     let test_config_input = CSVToEncodedDatasetBaseConfig{
         separator: b',',
         has_feature_names: true,
@@ -192,79 +219,28 @@ fn input_output_test() -> Result<(), UpdateError> {
     and.config_input.send(test_config_input)?;
     and.on_update()?;
 
-    let expected: Array2<u32> = array![[1, 2, 3, 1, 2, 3], [4, 5, 6, 1, 2, 3], [7, 8, 9, 1, 2, 3], [10, 8, 9, 1, 2, 3], [12, 8, 9, 1, 2, 3], [7, 8, 9, 1, 2, 3]];
+    let expected: Array2<u32> = array![[1, 2, 3, 1, 2, 11], [4, 5, 6, 1, 5, 3], [7, 8, 9, 1, 2, 3], [10, 8, 9, 1, 6, 4], [12, 8, 9, 1, 2, 3], [7, 8, 9, 1, 2, 3]];
     let actual: Array2<u32> = mock_output.next()?.records;
     
     Ok(assert!(expected == actual))
 }
 
-fn label_encode_ordinal(records: &Array2<String>) -> Array2<f64> {
-    let mut encoded_data = Vec::new();
-    let mut label_mappings: Vec<HashMap<String, usize>> = Vec::new();
+fn label_encode(input: &Vec<String>) -> Vec<f64> {
+    // Create a HashMap to store the mapping of unique strings to labels
+    let mut label_mapping: HashMap<&String, f64> = HashMap::new();
 
-    for col_idx in 0..records.shape()[1] {
-        let mut label_mapping = HashMap::new();
-        let mut encoded_column = Vec::new();
+    // Assign labels to unique strings
+    let mut label_counter = 0.0;
+    let labels: Vec<f64> = input
+        .iter()
+        .map(|s| {
+            *label_mapping.entry(s).or_insert_with(|| {
+                let label = label_counter;
+                label_counter += 1.0;
+                label
+            })
+        })
+        .collect();
 
-        for value in records.column(col_idx).iter() {
-            let len_before_insert = label_mapping.len();
-            let label = *label_mapping
-                .entry(value.clone())
-                .or_insert_with(|| len_before_insert);
-
-            encoded_column.push(label as f64);
-        }
-
-        label_mappings.push(label_mapping);
-        encoded_data.push(encoded_column);
-    }
-
-    let rows = records.shape()[0];
-    let cols = records.shape()[1];
-    let encoded_data: Vec<Vec<f64>> = encoded_data;
-
-    Array2::from_shape_vec((rows, cols), encoded_data.into_iter().flatten().collect()).unwrap()
-}
-
-
-fn one_hot_encode(records: &Array2<String>) -> (Array2<f64>, Vec<String>) {
-    let mut encoded_data: Vec<Vec<f64>> = Vec::new();
-    let mut feature_names = Vec::new();
-    let mut label_mappings: Vec<HashMap<String, usize>> = Vec::new();
-
-    for col_idx in 0..records.shape()[1] {
-        let mut label_mapping = HashMap::new();
-        let mut encoded_column = Vec::new();
-
-        for value in records.column(col_idx).iter() {
-            let label = *label_mapping
-                .entry(value.clone())
-                .or_insert_with(|| label_mapping.len());
-
-            encoded_column.push(label as f64);
-        }
-
-        label_mappings.push(label_mapping);
-
-        // Append feature names for the one-hot encoded column
-        let column_name = format!("{}_one_hot", col_idx);
-        feature_names.push(column_name);
-    }
-
-    // Create a sparse one-hot encoded array
-    let rows = records.shape()[0];
-    let cols = label_mappings.iter().map(|map| map.len()).sum();
-    let mut encoded_array = Array2::zeros((rows, cols));
-
-    for (col_idx, label_mapping) in label_mappings.iter().enumerate() {
-        for (row_idx, label) in records.column(col_idx).iter().zip(encoded_array.axis_iter_mut(Axis(0))) {
-            label_mapping.iter().for_each(|(&value, &encoded_label)| {
-                if label[0] == encoded_label as f64 {
-                    label.iter_mut().for_each(|x| *x = 1.0);
-                }
-            });
-        }
-    }
-
-    (encoded_array, feature_names)
+    labels
 }
