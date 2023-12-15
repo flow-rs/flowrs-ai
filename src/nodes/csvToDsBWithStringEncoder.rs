a8eecf2cdd311a184bcc1e98f68eb833d7f91c12
Bellhopx <temp_bellhop@outlook.com>
2023-12-15 23:35:26 +0100

    add one-hot encoding and concatonate array method

--- src/nodes/csvToDsBWithStringEncoder.rs
+++ src/nodes/csvToDsBWithStringEncoder.rs
@@ -4,7 +4,7 @@ use flowrs::RuntimeConnectable;
 
 use linfa::dataset::{DatasetBase, Float, Labels, Records};
 use linfa_kernel::Inner;
-use ndarray::{array, ArrayBase, OwnedRepr, Dim, Axis, Array1, Array2};
+use ndarray::{array, ArrayBase, OwnedRepr, Dim, Axis, Array1, Array2, s};
 use csv::ReaderBuilder;
 use ndarray_csv::Array2Reader;
 use serde::{Deserialize, Serialize, de::DeserializeOwned};
@@ -131,8 +131,19 @@ where
                     label_encoded_ordinals.push(label_encode(&ordinal));
                 }
 
+                // Example usage:
+                let mut one_hot_encoded_nominals: Vec<Array2<f64>> = Vec::new();
+                let mut one_hot_feature_names: Vec<String> = Vec::new();
+
+                // Assuming `nominal_data` and `config.nominals` are defined somewhere
+                for (nominal, feature_name) in nominal_data.iter().zip(config.nominals.iter()) {
+                    let (one_hot_encoding, feature_names) = one_hot_encode(nominal, feature_name.to_string());
+                    one_hot_encoded_nominals.push(one_hot_encoding);
+                    one_hot_feature_names.extend(feature_names);
+                }
+
                 // Converting Data to ndarrays:
-                let nominal_records: Array2<String> = Array2::from_shape_vec((nominal_data.len(), headers.len()), nominal_data.into_iter().flatten().collect()).unwrap();
+                let nominal_records = concatenate_arrays(one_hot_encoded_nominals);
                 let ordinal_records: Array2<f64> = Array2::from_shape_vec((label_encoded_ordinals.len(), headers.len()), label_encoded_ordinals.into_iter().flatten().collect()).unwrap();
                 let other_records: Array2<f64> = Array2::from_shape_vec((other_data.len(), headers.len()), other_data.into_iter().flatten().collect()).unwrap();
                                 
@@ -155,6 +166,7 @@ where
                 //let others = others_dbs.with_feature_names(config.others);
 
                 println!("Nominals: {:?}", nominal_records);
+                println!("Feature names: {:?}", one_hot_feature_names);
                 println!("Ordinal records: {:?}", ordinal_records);
                 //println!("Ordinals: {:?}", ordinals);
                 println!("Others: {:?}", other_records);
@@ -176,10 +188,16 @@ where
                 "goodbye".to_string()];
 
                 // Apply label encoding
-                let encoded_labels = label_encode(&data);
+                let encoded_labels: Vec<f64> = label_encode(&data.clone());
+
+                // Apply one-hot encoding
+                let (one_hot_encoding, feature_names) = one_hot_encode(&data, "text".to_string());
+
 
                 // Display the result
                 println!("Encoded Labels: {:?}", encoded_labels);
+                println!("Feature Names: {:?}", feature_names);
+                println!("One-Hot Encoding:\n{:?}", one_hot_encoding);
                 
 
 
@@ -243,4 +261,69 @@ fn label_encode(input: &Vec<String>) -> Vec<f64> {
         .collect();
 
     labels
+}
+
+fn one_hot_encode(input: &Vec<String>, feature_name: String) -> (Array2<f64>, Vec<String>) {
+    // Create a HashMap to store the mapping of unique strings to column indices
+    let mut column_mapping: HashMap<&String, usize> = HashMap::new();
+
+    // Assign column indices to unique strings
+    let mut column_counter = 0;
+    let mut feature_names = Vec::new();
+    for s in input.iter() {
+        if !column_mapping.contains_key(s) {
+            column_mapping.insert(s, column_counter);
+            let full_feature_name = format!("{}_{}", feature_name, s);
+            feature_names.push(full_feature_name);
+            column_counter += 1;
+        }
+    }
+
+    // Create a sparse one-hot encoding matrix
+    let rows = input.len();
+    let cols = column_counter;
+    let mut encoding: ArrayBase<OwnedRepr<f64>, Dim<[usize; 2]>> = Array2::zeros((rows, cols));
+
+    for (row_idx, s) in input.iter().enumerate() {
+        if let Some(&col_idx) = column_mapping.get(s) {
+            encoding[[row_idx, col_idx]] = 1.0;
+        }
+    }
+
+    (encoding, feature_names)
+}
+
+fn concatenate_arrays(mut arrays: Vec<Array2<f64>>) -> Array2<f64> {
+    // Check if the vector is not empty
+    if arrays.is_empty() {
+        panic!("Input vector is empty");
+    }
+
+    // Get the shape of the first array
+    let (rows, cols) = (arrays[0].shape()[0], arrays.iter().map(|arr| arr.shape()[1]).sum());
+
+    // Create a new Array2 to hold the concatenated data
+    let mut result = Array2::zeros((rows, cols));
+
+    // Starting column index in the result array
+    let mut col_index = 0;
+
+    // Iterate over the arrays and fill the result array
+    for array in arrays {
+        // Check if the array has the same number of rows as the first one
+        if array.shape()[0] != rows {
+            panic!("Inconsistent number of rows in arrays");
+        }
+
+        // Get the number of columns in the current array
+        let current_cols = array.shape()[1];
+
+        // Copy the data from the current array into the result
+        result.slice_mut(s![.., col_index..col_index + current_cols]).assign(&array);
+
+        // Update the column index for the next array
+        col_index += current_cols;
+    }
+
+    result
 }
