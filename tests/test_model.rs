7c0438ee617b5ae67fb4c208f6d9f780d9471263
Joshua Stalla <joshua.stalla@adesso.de>
2024-01-16 19:01:16 +0100

    refactor: remove uneccessary image load from model_node test

--- tests/test_model.rs
+++ tests/test_model.rs
@@ -3,7 +3,7 @@ mod nodes {
     use flowrs_ai::model::{ModelNode, ModelConfig};
     use flowrs::{node::{ChangeObserver, Node}, connection::{connect, Edge}};
 
-    use ndarray::{ArrayD, s};
+    use ndarray::{ArrayD, s, IxDyn};
     use std::{env};
     use image::{imageops::FilterType, ImageBuffer, Pixel, Rgb};
 
@@ -14,7 +14,9 @@ mod nodes {
             model_path: "src/models/opt-squeeze.onnx".to_string(),
             model_base64: "".to_string(),
         };
-        let model_input = load_image();
+        
+        let shape = [1, 3, 224, 224];
+        let model_input = ArrayD::<f32>::zeros(IxDyn(&shape));
 
         let change_observer: ChangeObserver = ChangeObserver::new();  
 
@@ -43,33 +45,6 @@ mod nodes {
         let _ = model_node.input_model_config.send(model_config.clone());
         let _ = model_node.on_update();
         Ok(assert!(path == model_node.model_config.unwrap().model_path))
-    }
-
-
-    fn load_image() -> ArrayD<f32> {
-        let image_path = env::current_dir()
-        .expect("Failed to obtain current directory")
-        .join("src/images/7.jpg");
-        println!("Image Path: {:?}", image_path);
-    
-        let image_buffer: ImageBuffer<Rgb<u8>, Vec<u8>> = image::open(image_path).unwrap().resize_to_fill(224, 224, FilterType::Nearest).to_rgb8();
-    
-        let mut array: ndarray::ArrayBase<ndarray::OwnedRepr<f32>, ndarray::Dim<[usize; 4]>> = ndarray::Array::from_shape_fn((1, 3, 224, 224), |(_, c, j, i)| {
-            let pixel = image_buffer.get_pixel(i as u32, j as u32);
-            let channels = pixel.channels();
-    
-            (channels[c] as f32) / 255.0
-        });
-    
-        let mean = [0.485, 0.456, 0.406];
-        let std = [0.229, 0.224, 0.225];
-        for c in 0..3 {
-            let mut channel_array = array.slice_mut(s![0, c, .., ..]);
-            channel_array -= mean[c];
-            channel_array /= std[c];
-        }
-    
-        array.into_dyn()
     }
 }
 
